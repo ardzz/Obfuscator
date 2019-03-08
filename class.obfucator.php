8f5d5ce38d86c5e477bfd630a488121376cf7433
Ardan Ardzz <reyardn@gmail.com>
2019-03-08 17:34:07 +0700

    Update class.obfucator.php

--- class.obfucator.php
+++ class.obfucator.php
@@ -8,11 +8,10 @@
 *
 **/
 
-$key = "0123456789abcdefghijklmnopqrstuvwxyz;`</>- |_=,.:ABCDEFGHIJKLMNOPQRSTUVWXYZ\/";
-
 class ardzz_obfuscator{
+	public $key = "0123456789abcdefghijklmnopqrstuvwxyz;`</>- |_=,.:ABCDEFGHIJKLMNOPQRSTUVWXYZ\/";
 	function UnObfuscate($str){
-		global $key;
+		$key = $this->key;
 		preg_match_all("/\[(.*?)\]/", $str, $match);
 		$keys = str_split($key, "1");
 		$output = null;
@@ -30,7 +29,7 @@ class ardzz_obfuscator{
 		}
 	}
 	function Obfuscate($string, $var_name){
-		global $key;
+		$key = $this->key;
 		$str = $key;
 		$output = NULL;
 		$var_name = "\${$var_name}";
@@ -43,7 +42,7 @@ class ardzz_obfuscator{
 		"OH !mine !mine",
 		"You make my heart break",
 		"You make my heart break, again",
-		"Ku jek ruh bayangmu nang kamar",
+		"Jek ruh bayangmu nang kamar",
 		];
 		foreach(str_split($string,1) as $input){
 			foreach (str_split($str, 1) as $key => $value) {
@@ -57,23 +56,5 @@ class ardzz_obfuscator{
 		return "((NULL)".implode("", $output).")";
 	}
 }
-
-// example 
-
-$str = "((NULL)./* -- OH baby i don't understand this --*/(NULL)/* -- You make my heart break, again -- */.$key/*//*/[10]./*/-'-/*/(NULL)./* -- OH baby i don't understand this --*/(NULL)/* -- Loving you is danger -- */.$key/*//*/[27]./*/-'-/*/(NULL)./* -- I still see your shadows in my room --*/(NULL)/* -- You are never mine -- */.$key/*//*/[13]./*/-'-/*/(NULL)./* -- I feel a-okay --*/(NULL)/* -- Loving you is danger -- */.$key/*//*/[35]./*/-'-/*/(NULL)./* -- I feel a-okay --*/(NULL)/* -- You are never mine -- */.$key/*//*/[35]./*/-'-/*/(NULL)./* -- I still see your shadows in my room --*/(NULL)/* -- You are never mine -- */.$key/*//*/[44]./*/-'-/*/(NULL)./* -- You make my heart break, again --*/(NULL)/* -- You make my heart break -- */.$key/*//*/[24]./*/-'-/*/(NULL)./* -- OH baby i don't understand this --*/(NULL)/* -- You make my heart break, again -- */.$key/*//*/[11]./*/-'-/*/(NULL)./* -- OH !mine !mine --*/(NULL)/* -- You make my heart break, again -- */.$key/*//*/[15]./*/-'-/*/(NULL)./* -- You make my heart break, again --*/(NULL)/* -- You are never mine -- */.$key/*//*/[30]./*/-'-/*/(NULL)./* -- You are never mine --*/(NULL)/* -- You make my heart break, again -- */.$key/*//*/[28]./*/-'-/*/(NULL)./* -- OH !mine !mine --*/(NULL)/* -- You are never mine -- */.$key/*//*/[12]./*/-'-/*/(NULL)./* -- OH baby i don't understand this --*/(NULL)/* -- OH !mine !mine -- */.$key/*//*/[10]./*/-'-/*/(NULL)./* -- OH baby i don't understand this --*/(NULL)/* -- Loving you is danger -- */.$key/*//*/[29]./*/-'-/*/(NULL)./* -- OH !mine !mine --*/(NULL)/* -- You are never mine -- */.$key/*//*/[24]./*/-'-/*/(NULL)./* -- OH baby i don't understand this --*/(NULL)/* -- OH baby i don't understand this -- */.$key/*//*/[27]./*/-'-/*/(NULL))";
-
-$ardzz_obfuscator = new ardzz_obfuscator();
-/*
-	 -- SAMPLE -- OBFUSCATE -- (ENCODE) --
-	 
-echo $ardzz_obfuscator->UnObfuscate($str, "key").PHP_EOL;
-
-
-	 -- SAMPLE -- UNOBFUSCATE -- (DECODE) --
-
-echo $ardzz_obfuscator->Obfuscate("example", "key").PHP_EOL;
-
-*/
-
-echo $ardzz_obfuscator->Obfuscate("example", "key").PHP_EOL;
+		
 ?>
